2604308aa21de8c448d8b67a5e14385eecfdff4c
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-09-01 14:47:49 -0700

    Update HelloWorld.java
    carted a fernchange method.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,9 @@
 public class HelloWorld {
+      public static void main(String[] args){
+
+    }
+    public static void tempChange(double feran){
+        double celc = 5 * (feran - 32) / 9;
+        System.out.println("feran: " + feran + "\n celc" + celc);
+    }
 }
