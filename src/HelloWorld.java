06ced4205a6f5b98a25f32e7ecd039720efd5f9b
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-09-01 16:55:23 -0700

    new update.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
-      public static void main(String[] args){
-
+    public static void main(String[] args){
+        tempChange(50);
     }
     public static void tempChange(double feran){
         double celc = 5 * (feran - 32) / 9;
