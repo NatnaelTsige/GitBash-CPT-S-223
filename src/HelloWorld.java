028893527e15dbf7f28146eb7b93113b0cdb9bab
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-09-01 17:00:09 -0700

    Update HelloWorld.java
    second method edited

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -9,5 +9,7 @@ public class HelloWorld {
     public static void tempChange2(double feran){
         double celc = 5 * (feran - 32) / 9;
         System.out.println("feran: " + feran + "\n celc" + celc);
+        double undo = ( (celc + 32) / 5 ) * 9;
+        System.out.println(undo);
     }
 }
