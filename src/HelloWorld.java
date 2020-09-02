64e6126c7ff07b620989e3e749fa976b69c6a1a9
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-09-01 19:13:27 -0700

    second day of class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,16 @@
 public class HelloWorld {
-    public static void main(String[] args){
-        tempChange(50);
-    }
+
     public static void tempChange(double feran){
         double celc = 5 * (feran - 32) / 9;
-        System.out.println("feran: " + feran + "\n celc" + celc);
+        System.out.println("feranheit: " + feran + "\n celcius" + celc + "\n");
     }
-    public static void tempChange2(double feran){
-        double celc = 5 * (feran - 32) / 9;
-        System.out.println("feran: " + feran + "\n celc" + celc);
-        double undo = ( (celc + 32) / 5 ) * 9;
-        System.out.println(undo);
+
+    public static void main(String[] args){
+        System.out.println("Hello World!\n");
+
+        double f = 2.4;
+        tempChange(5.6);
+        tempChange(f);
     }
+
 }
